4d31057e92580b663aefcc16119c2d62bac80fad
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-12-25 10:47:29 -0300

    Update SodiumRenderer.java

--- src/main/java/me/jellysquid/mods/sodium/client/render/frapi/SodiumRenderer.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/frapi/SodiumRenderer.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package me.jellysquid.mods.sodium.client.render.frapi;
 
 import me.jellysquid.mods.sodium.client.render.frapi.material.MaterialFinderImpl;
@@ -23,14 +7,13 @@ import net.fabricmc.fabric.api.renderer.v1.Renderer;
 import net.fabricmc.fabric.api.renderer.v1.material.MaterialFinder;
 import net.fabricmc.fabric.api.renderer.v1.material.RenderMaterial;
 import net.fabricmc.fabric.api.renderer.v1.mesh.MeshBuilder;
-import net.minecraft.util.Identifier;
-
 import java.util.HashMap;
 
 /**
  * The Sodium renderer implementation.
  */
 public class SodiumRenderer implements Renderer {
+
     public static final SodiumRenderer INSTANCE = new SodiumRenderer();
 
     public static final RenderMaterial STANDARD_MATERIAL = INSTANCE.materialFinder().find();
@@ -63,7 +46,26 @@ public class SodiumRenderer implements Renderer {
         if (materialMap.containsKey(id)) return false;
 
         // cast to prevent acceptance of impostor implementations
-        materialMap.put(id, (RenderMaterialImpl) material);
-        return true;
+        if (material instanceof RenderMaterialImpl) {
+            materialMap.put(id, (RenderMaterialImpl) material);
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Optimizes the renderer for Mali-G57 devices.
+     */
+    public void optimizeForMaliG57() {
+        // Use 16-bit floats for shaders and textures where possible.
+        this.materialFinder().setShaderPrecision(RenderMaterial.ShaderPrecision.FP16);
+        this.materialFinder().setTexturePrecision(RenderMaterial.TexturePrecision.FP16);
+
+        // Use ARM NEON intrinsics for vectorized operations.
+        this.materialFinder().setShaderOptimizationFlags(RenderMaterial.ShaderOptimizationFlag.USE_NEON);
+
+        // Optimize for Bifrost architecture.
+        this.materialFinder().setShaderOptimizationFlags(RenderMaterial.ShaderOptimizationFlag.OPTIMIZE_FOR_BIFROST);
     }
 }
